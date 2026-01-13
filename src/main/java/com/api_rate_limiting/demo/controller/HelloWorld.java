50d600327912e407a1a327d6bf0a8eb55804c55d
Shashank-MP <shashankmechana2001@gmail.com>
2026-01-12 22:15:57 -0600

    Add request logging filter using OncePerRequestFilter

---
+++ src/main/java/com/api_rate_limiting/demo/controller/HelloWorld.java
@@ -0,0 +1,13 @@
+package com.api_rate_limiting.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorld {
+
+    @GetMapping("/api/hello")
+    public String Hello(){
+        return "hello";
+    }
+}
